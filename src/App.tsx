8f389a8eb3c0ea48ca9640f3452fcba124a0dc01
dalogsamurai <thelogsamurai@gmail.com>
2025-12-06 02:06:58 +0300

    feat: supabase add

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { supabase } from "./utils/supabase";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // TODO add type
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    let { data: users, error } = await supabase.from("users").select("id");
+
+    console.log(users);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
